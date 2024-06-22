d4b085adb5e6406d69041799f7d47f11b0b0711b
Sherlock Xu <65327072+Sherlock113@users.noreply.github.com>
2024-06-22 18:55:38 +0800

    Update main.py

--- password-manager/main.py
+++ password-manager/main.py
@@ -3,6 +3,7 @@ from tkinter import messagebox
 import secrets
 import string
 import pyperclip
+import json
 
 PASSWORD_LENGTH = [12, 13, 14, 15]
 
@@ -25,9 +26,9 @@ email.grid(row=2, column=0)
 password = Label(text="Password:")
 password.grid(row=3, column=0)
 
-website_entry = Entry(width=38)
-website_entry.grid(row=1, column=1, columnspan=2)
-website_entry.focus()  # local the initial cursor
+website_entry = Entry(width=21)
+website_entry.grid(row=1, column=1)
+website_entry.focus()  # Locate the initial cursor
 email_entry = Entry(width=38)
 email_entry.grid(row=2, column=1, columnspan=2)
 email_entry.insert(0, "example@gmail.com")
@@ -43,33 +44,65 @@ def generate():
     pyperclip.copy(password)
 
 
-create_password = Button(text="Generate Password", command=generate)
-create_password.grid(row=3, column=2)
+def find_password():
+    website_info = website_entry.get()
+    try:
+        with open("data.json", mode="r") as file:
+            data = json.load(file)
+    except FileNotFoundError:
+        messagebox.showwarning(title="Error",
+                               message="No data file found.")
+    else:
+        if website_info in data:
+            messagebox.showinfo(title=website_info,
+                                message=f"Email: {data[website_info]['email']}\nPassword: {data[website_info]['password']}")
+        else:
+            messagebox.showwarning(title=website_info,
+                                   message="No details for the website found.")
 
 
 def save():
-    website_info = website_entry.get()
     email_info = email_entry.get()
     password_info = password_entry.get()
+    website_info = website_entry.get()
+    new_data = {
+        website_info: {
+            "email": email_info,
+            "password": password_info,
+        }
+    }
 
     if website_info == "" or email_info == "" or password_info == "":
         messagebox.showwarning(title="oops", message="Please don't leave any fields empty.")
     else:
-        info_to_add = website_info + " | " + email_info + " | " + password_info
+        try:
+            with open("data.json", mode="r") as file:
+                # Read the old data
+                data = json.load(file)
+        except FileNotFoundError:
+            with open("data.json", mode="w") as file:
+                # Put the new data in the file
+                json.dump(new_data, file, indent=4)
 
-        is_ok = messagebox.askokcancel(title=website_info,
-                                       message=f"Account info: \nEmail: {email_entry.get()} \nPassword: {password_entry.get()} \nIs it ok to save?")
-
-        if is_ok:
-            with open("data.txt", mode="a") as file:
-                file.write(f"{info_to_add}\n")
                 password_entry.delete(0, 'end')
                 website_entry.delete(0, 'end')
+        else:
+            data.update(new_data)
+
+            with open("data.json", mode="w") as file:
+                # Put the new data in the file
+                json.dump(data, file, indent=4)
+        finally:
+            password_entry.delete(0, 'end')
+            website_entry.delete(0, 'end')
 
 
 add_password = Button(text="Add", width=36, command=save)
-
 add_password.grid(row=4, column=1, columnspan=2)
+create_password = Button(text="Generate Password", command=generate)
+create_password.grid(row=3, column=2)
+search_password = Button(text="Search", command=find_password, width=13)
+search_password.grid(row=1, column=2)
 
 # Set a loop in case the window closes
 window.mainloop()
